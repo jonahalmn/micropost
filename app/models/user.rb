6016fa933e51866a627e928a87f72a96e9515640
jonahalmn <jonah.allemonne@icloud.com>
2017-01-06 12:11:03 +0100

    User page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,14 @@
 #  updated_at :datetime         not null
 #
 
-email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 
 class User < ApplicationRecord
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :nom, :presence => true,
             :length => {:maximum => 50}
 
   validates :email, :presence => true,
-            :format   => { :with => email_regex },
+            :format   => email_regex ,
             :uniqueness => {:case_sensitive => false}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/signup', to: 'users#new'
 
   get '/', to: 'pages#home'
+
   get 'pages/contact'
 
   get 'pages/about'
 
   get 'pages/help'
 
+get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
